38484af9578863996cdfd6d8597f700b9a5dfb59
Hazim Awad <66192633+Haz-Thunder@users.noreply.github.com>
2020-09-17 17:55:31 -0700

    Started Space Shooter project
    Created Player movment and shooting mechanics

---
+++ Space Shooter/Assets/Scripts/Player.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    [SerializeField]
+    private float _speed = 5.5f;
+    [SerializeField]
+    private GameObject _laserPrefab;
+    [SerializeField]
+    private float _fireRate = 0.15f;
+    private float _canFire = -1.0f;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        transform.position = new Vector3(0, 0, 0);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        CalculateMovement();
+
+        if (Input.GetKeyDown(KeyCode.Space) && Time.time > _canFire)
+        {
+            FireLaser();
+        }
+    }
+
+    void CalculateMovement()
+    {
+        // set up inputs for movement 
+        float horizontalInput = Input.GetAxis("Horizontal");
+        float VerticalInput = Input.GetAxis("Vertical");
+
+        // the value of the movment 
+        transform.Translate(new Vector3(horizontalInput, VerticalInput, 0) * _speed * Time.deltaTime);
+
+        // restrict the vertical movment 
+        transform.position = new Vector3(transform.position.x, Mathf.Clamp(transform.position.y, -3.8f, 0), 0);
+
+        // restrict the horizontal movemnt
+        if (transform.position.x >= 11.3f)
+        {
+            transform.position = new Vector3(-11.3f, transform.position.y, 0);
+        }
+        else if (transform.position.x <= -11.3f)
+        {
+            transform.position = new Vector3(11.3f, transform.position.y, 0);
+        }
+    }
+
+    void FireLaser()
+    {
+        _canFire = Time.time + _fireRate; // Fire cool down
+        Instantiate(_laserPrefab, (transform.position + new Vector3(0, 0.8f, 0)), Quaternion.identity);
+    }
+}
